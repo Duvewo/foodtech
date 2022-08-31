2ae3fefa4915800b48985e56bf852f612083c525
Duvewo <duvewo@gmail.com>
2022-08-31 12:29:34 +0500

    added: food category, padding for appbar row and food info

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:foodtech/screens/home.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowMaterialGrid: false,
+    debugShowCheckedModeBanner: false,
     initialRoute: "/",
     routes: {
       "/": (BuildContext context) => const HomePageClass(),
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -21,49 +21,72 @@ class HomePageClassState extends State<HomePageClass> {
                   end: Alignment.bottomCenter)),
           child: Column(
             children: [
-              const Padding(padding: EdgeInsets.all(20)),
-              Row(children: [
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-                  const Text(
-                    "Delivery address",
-                    style: TextStyle(color: Colors.grey, fontSize: 15),
-                  ),
-                  Row(mainAxisSize: MainAxisSize.min, children: const [
-                    Text(
-                      "Address, 1",
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                    ),
-                    IconButton(
-                        onPressed: null,
-                        icon: Icon(Icons.edit_rounded),
-                        iconSize: 15.0)
-                  ])
-                ]),
-                const Spacer(),
-                Row(children: [
-                  Container(
-                      clipBehavior: Clip.antiAlias,
-                      decoration: const BoxDecoration(
-                          color: Colors.white, shape: BoxShape.circle),
-                      child: const IconButton(
-                          onPressed: null,
-                          icon: Icon(
-                            Icons.search_rounded,
-                            color: Colors.black,
+              Padding(
+                  padding: EdgeInsets.only(left: 20, right: 20, top: 40),
+                  child: Row(children: [
+                    Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "Delivery address",
+                            style: TextStyle(color: Colors.grey, fontSize: 15),
                           ),
-                          color: Colors.white)),
-                  const SizedBox(width: 10),
-                  Container(
-                      clipBehavior: Clip.antiAlias,
-                      decoration: const BoxDecoration(
-                          color: Colors.white, shape: BoxShape.circle),
-                      child: const IconButton(
-                          onPressed: null,
-                          icon: Icon(Icons.person_rounded, color: Colors.black),
-                          color: Colors.white))
-                ]),
-              ]),
+                          Row(mainAxisSize: MainAxisSize.min, children: const [
+                            Text(
+                              "Address, 1",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 18),
+                            ),
+                            IconButton(
+                                onPressed: null,
+                                icon: Icon(Icons.edit_rounded),
+                                iconSize: 15.0)
+                          ])
+                        ]),
+                    const Spacer(),
+                    Row(children: [
+                      Container(
+                          clipBehavior: Clip.antiAlias,
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              shape: BoxShape.circle,
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(0.3),
+                                  spreadRadius: 3,
+                                  blurRadius: 3,
+                                  offset: const Offset(0, 1),
+                                )
+                              ]),
+                          child: const IconButton(
+                              onPressed: null,
+                              icon: Icon(
+                                Icons.search_rounded,
+                                color: Colors.black,
+                              ),
+                              color: Colors.white)),
+                      const SizedBox(width: 10),
+                      Container(
+                          clipBehavior: Clip.antiAlias,
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              shape: BoxShape.circle,
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(0.3),
+                                  spreadRadius: 3,
+                                  blurRadius: 3,
+                                  offset: const Offset(0, 1),
+                                )
+                              ]),
+                          child: const IconButton(
+                              onPressed: null,
+                              icon: Icon(Icons.person_rounded,
+                                  color: Colors.black),
+                              color: Colors.white))
+                    ]),
+                  ])),
+              Padding(padding: EdgeInsets.all(10)),
               //FOOD CARDS
               SingleChildScrollView(
                   scrollDirection: Axis.horizontal,
@@ -93,6 +116,25 @@ class HomePageClassState extends State<HomePageClass> {
                   )
                 ],
               ),
+              Padding(
+                  padding: const EdgeInsets.only(left: 30, top: 20),
+                  child: SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: [
+                          _buildFoodCategoryButton(),
+                          Padding(padding: EdgeInsets.all(7)),
+                          _buildFoodCategoryButton(),
+                          Padding(padding: EdgeInsets.all(7)),
+                          _buildFoodCategoryButton(),
+                          Padding(padding: EdgeInsets.all(7)),
+                          _buildFoodCategoryButton(),
+                          Padding(padding: EdgeInsets.all(7)),
+                          _buildFoodCategoryButton(),
+                          Padding(padding: EdgeInsets.all(7)),
+                          _buildFoodCategoryButton(),
+                        ],
+                      )))
             ],
           ),
         ));
@@ -103,8 +145,8 @@ Widget _buildFoodCard() {
   return Container(
       clipBehavior: Clip.antiAlias,
       decoration: const BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(30))),
-      height: 270,
+          borderRadius: BorderRadius.all(Radius.circular(40))),
+      height: 280,
       width: 320,
       child: Stack(
         alignment: Alignment.topCenter,
@@ -121,35 +163,66 @@ Widget _buildFoodCard() {
                     borderRadius: BorderRadius.all(Radius.circular(5))),
                 height: 70,
                 width: 260,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      "Spotted dick",
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                    ),
-                    const Text(
-                      "Traditional British steamed pudding",
-                      style: TextStyle(color: Colors.grey),
-                    ),
-                    const Padding(padding: EdgeInsets.all(5)),
-                    Row(
-                      children: const [
-                        Icon(
-                          Icons.star_rounded,
-                          color: Colors.orangeAccent,
+                child: Padding(
+                    padding: EdgeInsets.only(left: 20, top: 5),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          "Spotted dick",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 20),
                         ),
-                        Text("4.9 (284)"),
-                        Icon(Icons.timelapse_rounded, color: Colors.grey),
-                        Text("15-20 mins"),
-                        Icon(Icons.attach_money_rounded, color: Colors.grey),
-                        Text("Free")
+                        const Text(
+                          "Traditional British steamed pudding",
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                        const Padding(padding: EdgeInsets.all(5)),
+                        Row(
+                          children: const [
+                            Icon(
+                              Icons.star_rounded,
+                              color: Colors.orangeAccent,
+                            ),
+                            Text("4.9 (284)"),
+                            Icon(Icons.timelapse_rounded, color: Colors.grey),
+                            Text("15-20 mins"),
+                            Icon(Icons.attach_money_rounded,
+                                color: Colors.grey),
+                            Text("Free")
+                          ],
+                        )
                       ],
-                    )
-                  ],
-                ),
+                    )),
               ))
         ],
       ));
+}
+
+Widget _buildFoodCategoryButton() {
+  return Column(
+    children: [
+      Container(
+          clipBehavior: Clip.antiAlias,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              shape: BoxShape.circle,
+              boxShadow: [
+                BoxShadow(
+                  blurRadius: 12,
+                  spreadRadius: 4,
+                  color: Colors.grey.withOpacity(0.3),
+                  offset: Offset(0, 5),
+                ),
+              ]),
+          child: const IconButton(
+              onPressed: null,
+              icon: Icon(
+                Icons.fastfood_outlined,
+                color: Color(0xFFEEC230),
+              ))),
+      const Padding(padding: EdgeInsets.all(3)),
+      const Text("American")
+    ],
+  );
 }
